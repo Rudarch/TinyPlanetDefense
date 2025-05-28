be817155bbd3b4f67a10de167fab17c17a9fd78e
Yurii <yurii.rudyk@gmail.com>
2025-05-28 12:34:31 +0300

    A new Beginning

--- Assets/Scripts/UpgradeGrade.cs
+++
@@ -1,6 +0,0 @@
-public enum UpgradeGrade
-{
-    Common,
-    Rare,
-    Epic
-}
--- Assets/Scripts/UpgradeType.cs
+++
@@ -1,8 +0,0 @@
-public enum UpgradeType
-{
-    FireCooldown,
-    Damage,
-    RotationSpeed,
-    FiringRange,
-    MaxHealth
-}
